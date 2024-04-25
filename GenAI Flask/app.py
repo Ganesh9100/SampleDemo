98bd06ff81eacc75779d285752e9d150380c7503
Ganesh Raj V <60533730+Ganesh9100@users.noreply.github.com>
2024-04-25 14:54:33 +0530

    Add files via upload

---
+++ GenAI Flask/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify
+from ai21 import AI21Client
+
+app = Flask(__name__)
+
+
+client = AI21Client(api_key="DincWiO8L70416GKy6RCKziBiCQwMFlS")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        content = request.form['content']
+        question = request.form['question']
+        prompt = f"Respond to my Question, based on the given content. Content : {content} Question : {question}."
+        genaires = answerr_genai(prompt)
+        return str(genaires)
+    return render_template('index.html')
+
+def answerr_genai(prompt):
+    # Call AI21 API to summarize text
+    response= client.completion.create(
+    model="j2-ultra",
+    prompt=prompt,
+    num_results=1,
+    max_tokens=200,
+    temperature=0.1,)
+    
+    # st = response['completions'][0]['data']['text']
+    # st = response['completions'][0]['data']['text']
+    st = response.completions[0].data.text
+    return st
+
+if __name__ == '__main__':
+    app.run(debug=True)
